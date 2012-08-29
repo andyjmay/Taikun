63e7ca3a0e8ce21a5f8588e4a96dcaee84b3f465
Andy May <andyjmay@gmail.com>
2012-08-29 16:34:20 -0400

    API Change - Unit test update
    Moved to IEnumerable instead of IQueryable.
    Added nuget package restore

--- Taikun.SqlServer/SqlServerProjectManager.cs
+++ Taikun.SqlServer/SqlServerProjectManager.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace Taikun.SqlServer {
   public class SqlServerProjectManager : IProjectManager {
-    private string connectionString;
+    private SqlConnectionStringBuilder connectionStringBuilder;
 
-    public SqlServerProjectManager(string connectionString) {
-      this.connectionString = connectionString;
+    public SqlServerProjectManager(string connectionString, bool createIfNotExists = false) {
+      connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+      if (createIfNotExists && !databaseExists(connectionStringBuilder.InitialCatalog)) {
+        createDatabase(connectionStringBuilder.InitialCatalog);
+        string createTableCommand = "CREATE TABLE [dbo].[Projects]([ID] [int] IDENTITY(1,1) NOT NULL,[DatabaseName] [varchar](255) NOT NULL,[Description] [varchar](255) NULL)";
+        using (var connection = new SqlConnection(getDatabaseConnectionString(connectionStringBuilder.InitialCatalog))) {
+          using (var command = new SqlCommand(createTableCommand, connection)) {
+            connection.Open();
+            command.ExecuteNonQuery();
+          }
+        }
+      }
     }
 
-    public IQueryable<IProject> GetAllProjects() {
+    public IEnumerable<IProject> GetAllProjects() {
       var projects = new List<IProject>();
       string queryString = "SELECT ID, DatabaseName, Description FROM Projects";
 
-      using (SqlConnection connection = new SqlConnection(connectionString)) {       
-        using (SqlCommand command = new SqlCommand(queryString, connection)) {
+      using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString)) {       
+        using (var command = new SqlCommand(queryString, connection)) {
           connection.Open();
 
           SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
@@ -31,13 +39,32 @@ namespace Taikun.SqlServer {
           }
         }      
       }
-      return projects.AsQueryable();
+      return projects;
+    }
+
+    public IProject GetProject(string databaseName) {
+      string queryString = "SELECT ID, DatabaseName, Description FROM Projects WHERE DatabaseName=@DatabaseName";
+
+      using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString)) {
+        using (var command = new SqlCommand(queryString, connection)) {
+          command.Parameters.AddWithValue("@DatabaseName", databaseName);
+          connection.Open();
+          SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+          dataReader.Read();
+          return new SqlServerProject {
+            Id = Convert.ToInt32(dataReader["ID"]),
+            DatabaseName = dataReader["DatabaseName"].ToString(),
+            Description = dataReader["Description"].ToString()
+          };
+        }
+      }
     }
 
     public IProject CreateProject(IProject project) {
       string insertCommand = "INSERT INTO Projects (DatabaseName, Description) Values (@DatabaseName, @Description); SELECT SCOPE_IDENTITY()";
-      using (SqlConnection connection = new SqlConnection(connectionString)) {
-        using (SqlCommand command = new SqlCommand(insertCommand, connection)) {
+      createDatabase(project.DatabaseName);
+      using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString)) {
+        using (var command = new SqlCommand(insertCommand, connection)) {
           command.Parameters.AddWithValue("@DatabaseName", project.DatabaseName);
           command.Parameters.AddWithValue("@Description", project.Description);
           connection.Open();
@@ -52,11 +79,76 @@ namespace Taikun.SqlServer {
     }
 
     public IProject UpdateProject(IProject project) {
-      throw new NotImplementedException();
+      string updateCommand = "UPDATE Projects SET DatabaseName=@DatabaseName, Description=@Description WHERE DatabaseName=@DatabaseName";
+      using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString)) {
+        using (var command = new SqlCommand(updateCommand, connection)) {
+          command.Parameters.AddWithValue("@DatabaseName", project.DatabaseName);
+          command.Parameters.AddWithValue("@Description", project.Description);
+          connection.Open();
+          command.ExecuteNonQuery();
+        }
+      }
+      return project;
     }
 
     public void DeleteProject(IProject project) {
-      throw new NotImplementedException();
+      string deleteProjectCommand = string.Format("DELETE FROM [Projects] WHERE [DatabaseName]='{0}'", project.DatabaseName);
+      using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString)) {
+        using (var command = new SqlCommand(deleteProjectCommand, connection)) {
+          connection.Open();
+          command.ExecuteNonQuery();
+        }
+      }
+      dropDatabase(project.DatabaseName);
+    }
+
+    private void createDatabase(string databaseName) {
+      string createDatabaseCommand = string.Format("CREATE DATABASE [{0}]", databaseName);
+      using (var connection = new SqlConnection(getMasterDatabaseConnectionString())) {
+        using (var command = new SqlCommand(createDatabaseCommand, connection)) {
+          connection.Open();
+          command.ExecuteNonQuery();
+        }
+      }
+    }
+
+    private void dropDatabase(string databaseName) {
+      string dropDatabaseCommand = string.Format("DROP DATABASE [{0}]", databaseName);
+      using (var connection = new SqlConnection(getMasterDatabaseConnectionString())) {
+        using (var command = new SqlCommand(dropDatabaseCommand, connection)) {
+          connection.Open();
+          command.ExecuteNonQuery();
+        }
+      }
+    }
+
+    private bool databaseExists(string databaseName) {
+      using (var connection = new SqlConnection(getMasterDatabaseConnectionString())) {
+        string queryString = "SELECT Count(*) FROM sys.databases WHERE [name] = '" + databaseName + "'";
+        using (var command = new SqlCommand(queryString, connection)) {
+          connection.Open();
+          try {
+            int result = (int)command.ExecuteScalar();
+            return result > 0;
+          }
+          catch (SqlException ex) {
+            Console.WriteLine(ex);
+            return false;
+          }
+        }
+      }
+    }
+
+    private string getMasterDatabaseConnectionString() {
+      var builder = new SqlConnectionStringBuilder(connectionStringBuilder.ConnectionString);
+      builder.InitialCatalog = "master";
+      return builder.ConnectionString;
+    }
+
+    private string getDatabaseConnectionString(string databaseName) {
+      var builder = new SqlConnectionStringBuilder(connectionStringBuilder.ConnectionString);
+      builder.InitialCatalog = databaseName;
+      return builder.ConnectionString;
     }
   }
 }
--- Taikun/IProjectManager.cs
+++ Taikun/IProjectManager.cs
@@ -1,8 +1,9 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace Taikun {
   public interface IProjectManager {
-    IQueryable<IProject> GetAllProjects();
+    IEnumerable<IProject> GetAllProjects();
+    IProject GetProject(string databaseName);
     IProject CreateProject(IProject project);    
     IProject UpdateProject(IProject project);
     void DeleteProject(IProject project);
