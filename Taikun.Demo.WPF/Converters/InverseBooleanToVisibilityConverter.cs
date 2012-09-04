87bd9acb9c0fb9c1f368fbc4a71453164ed3771e
Andy May <andyjmay@gmail.com>
2012-09-03 22:16:11 -0400

    Updates UI

---
+++ Taikun.Demo.WPF/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Taikun.Demo.WPF.Converters {
+  public class InverseBooleanToVisibilityConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+      if (value is Boolean) {
+        return !(bool)value;
+      }
+      return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+      throw new NotImplementedException();
+    }
+  }
+}
--- Taikun.Demo.WPF/ViewModels/ProjectTablesViewModel.cs
+++ Taikun.Demo.WPF/ViewModels/ProjectTablesViewModel.cs
@@ -43,12 +43,34 @@ namespace Taikun.Demo.WPF.ViewModels {
       }
     }
 
+    private bool creatingNewTable;
+    public bool CreatingNewTable {
+      get { return creatingNewTable; }
+      set {
+        creatingNewTable = value;
+        RaisePropertyChanged(() => CreatingNewTable);
+      }
+    }
+    
+    private string newTableName;
+    public string NewTableName {
+      get { return newTableName; }
+      set {
+        newTableName = value;
+        RaisePropertyChanged(() => NewTableName);
+      }
+    }
+
     public ObservableCollection<IProjectTable> Tables { get; private set; }
 
     public RelayCommand LoadTableData { get; private set; }
+    public RelayCommand NewTable { get; private set; }
+    public RelayCommand CancelCreateNewTable { get; private set; }
+    public RelayCommand CreateNewTable { get; private set; }
     
     public ProjectTablesViewModel(IProjectManager projectManager) {
       this.projectManager = projectManager;
+      CreatingNewTable = false;
       if (!IsInDesignMode) {
         Tables = new ObservableCollection<IProjectTable>();
       } else {
@@ -57,12 +79,30 @@ namespace Taikun.Demo.WPF.ViewModels {
         };
       }
       LoadTableData = new RelayCommand(loadProjectTableData, () => { return (SelectedTable != null); });
+      NewTable = new RelayCommand(addNewTable, () => { return (SelectedProject != null); });
+      CancelCreateNewTable = new RelayCommand(cancelCreateNewTable, () => { return (CreatingNewTable); });
+      CreateNewTable = new RelayCommand(createNewTable, () => { return CreatingNewTable && !string.IsNullOrWhiteSpace(NewTableName);});
       Messenger.Default.Register<Events.ProjectSelected>(this, projectSelectedEventHandler);
     }
 
     private void loadProjectTableData() {
       IProjectTable projectTableWithData = projectManager.GetProjectTable(SelectedProject, SelectedTable.Name, true);
       SelectedTableData = ((SqlServerProjectTable)projectTableWithData).Schema;
+    }
+
+    private void addNewTable() {
+      //SelectedTable = new SqlServerProjectTable(new DataTable());
+      CreatingNewTable = true;
+    }
+
+    private void cancelCreateNewTable() {
+      CreatingNewTable = false;
+      NewTableName = string.Empty;
+    }
+
+    private void createNewTable() {
+      //projectManager.CreateProjectTable(SelectedProject, new SqlServerProjectTable(new DataTable(NewTableName)));
+      //projectSelectedEventHandler(SelectedProject);
     }
 
     private void projectSelectedEventHandler(Events.ProjectSelected projectSelectedEvent) {
